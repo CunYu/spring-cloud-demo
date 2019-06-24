27da035e0123a7d78dbdb616ac6c9ab21687a728
CunYu <541085864@qq.com>
2019-06-25 00:35:35 +0800

    eureka-ribbon-consumer模块RibbonClient配置优化。

--- eureka-ribbon-consumer/src/main/java/pers/cunyu/eurekaribbonconsumer/config/EurekaProviderRibbonClient.java
+++
@@ -1,9 +0,0 @@
-package pers.cunyu.eurekaribbonconsumer.config;
-
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RibbonClient(name = "eurekaProvider", configuration = EurekaProviderRibbonConfig.class)
-public class EurekaProviderRibbonClient {
-}
