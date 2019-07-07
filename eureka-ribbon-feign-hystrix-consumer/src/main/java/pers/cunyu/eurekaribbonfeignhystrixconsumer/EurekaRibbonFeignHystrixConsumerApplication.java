476c917a4ac17f962802bd3042ceaacac60d9230
CunYu <541085864@qq.com>
2019-07-07 23:57:08 +0800

    eureka-ribbon-feign-hystrix-consumer模块去除无用的注解和依赖包。

--- eureka-ribbon-feign-hystrix-consumer/src/main/java/pers/cunyu/eurekaribbonfeignhystrixconsumer/EurekaRibbonFeignHystrixConsumerApplication.java
+++ eureka-ribbon-feign-hystrix-consumer/src/main/java/pers/cunyu/eurekaribbonfeignhystrixconsumer/EurekaRibbonFeignHystrixConsumerApplication.java
@@ -2,11 +2,9 @@ package pers.cunyu.eurekaribbonfeignhystrixconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableHystrix
 @SpringBootApplication
 public class EurekaRibbonFeignHystrixConsumerApplication {
 
